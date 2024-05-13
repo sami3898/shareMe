5490c5ed7725def45617d85ef8443f191aef5ec3
Abdulsami <32996863+sami3898@users.noreply.github.com>
2024-05-13 18:03:22 +0530

    - added alert message

--- src/utils/ApiHelper.ts
+++ src/utils/ApiHelper.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import * as FileSystem from 'expo-file-system'
+import { Alert } from "react-native";
 
 const BASE_URL = "https://file.io/";
 
@@ -10,13 +10,6 @@ export const uploadFileApi = async(fileUri: any, fileName: string) => {
       form.append("file", { name: fileName, uri: fileUri, type: 'image/jpeg'} as any);
 
       const res = await axios.post(BASE_URL, form, {
-        onUploadProgress: ({ loaded, total }) => {
-            // return progressEvent.loaded
-            // console.log({ progressEvent })
-            const progress = Math.round((loaded / total) * 100);
-            console.log(`Upload Progress: ${progress}%`);
-            // console.log("UPLOAD PROGRESS ", progressEvent.loaded)
-        },
         headers: {
           accept: "application/json",
           Authorization: `Bearer ${process.env.AUTH_TOKEN}`,
@@ -26,10 +19,8 @@ export const uploadFileApi = async(fileUri: any, fileName: string) => {
       });
 
       return res.data;
-
-      // console.log("API RES ", JSON.stringify(res.data));
     } catch (error) {
-      console.log("Error ", JSON.stringify(error));
+      Alert.alert("Error", error?.response?.data)
     }
     
 }
