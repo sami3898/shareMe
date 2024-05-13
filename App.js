25d3510a3edd3282439264f49641798137308fc7
Abdulsami <32996863+sami3898@users.noreply.github.com>
2024-05-13 18:48:44 +0530

    - added splash screen and icon

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
 import HomeScreen from './src/screens/HomeScreen';
 import { useFonts } from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback, useEffect, useState } from 'react';
+import { View } from 'react-native';
+import { COLORS } from './src/utils/Constant';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+
 
   const [fontsLoaded, fontError] = useFonts({
     'Poppins-Regular': require('./assets/fonts/Poppins-Regular.ttf'),
@@ -9,10 +16,27 @@ export default function App() {
     'Poppins-Bold': require('./assets/fonts/Poppins-Bold.ttf'),
   })
 
+  useEffect(() => {
+
+    async function loaded() {
+      if (fontsLoaded) {
+        // This tells the splash screen to hide immediately! If we call this after
+        // `setAppIsReady`, then we may see a blank screen while the app is
+        // loading its initial state and rendering its first pixels. So instead,
+        // we hide the splash screen once we know the root view has already
+        // performed layout.
+        await SplashScreen.hideAsync();
+      }
+    }
+
+    loaded()
+    
+  }, [fontsLoaded]);
+
   if (!fontsLoaded && !fontError) {
     return null;
   }
 
-  return <HomeScreen />
+  return (<HomeScreen />)
 }
 
