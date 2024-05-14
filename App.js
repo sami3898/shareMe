5c86e21cc06a4e32ef28b6a6e62f9e55a28d62da
Abdulsami <32996863+sami3898@users.noreply.github.com>
2024-05-14 17:28:47 +0530

    - update spalsh screen

--- App.js
+++ App.js
@@ -20,11 +20,6 @@ export default function App() {
 
     async function loaded() {
       if (fontsLoaded) {
-        // This tells the splash screen to hide immediately! If we call this after
-        // `setAppIsReady`, then we may see a blank screen while the app is
-        // loading its initial state and rendering its first pixels. So instead,
-        // we hide the splash screen once we know the root view has already
-        // performed layout.
         await SplashScreen.hideAsync();
       }
     }
